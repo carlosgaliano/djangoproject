02d71590b569da6f9a9ff57589878b5888b4793b
Carlos Galiano <galiano111@gmail.com>
2024-10-22 17:31:07 -0500

    vista polls

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question 
-
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by("*-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")
     output = ", ".join([q.question_text for q in latest_question_list])
     # return HttpResponse("Hello, world. You're at the polls index")
     return HttpResponse(output)
